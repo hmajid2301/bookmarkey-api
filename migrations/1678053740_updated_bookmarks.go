4f7012f99b0fbebc9d05c69b1eb1fa2d2fe86c99
Haseeb Majid <hello@haseebmajid.dev>
2023-03-05 22:04:08 +0000

    fix: make collection not required on bookmark
    Relates to: https://gitlab.com/bookmarkey/gui/-/issues/159

---
+++ migrations/1678053740_updated_bookmarks.go
@@ -0,0 +1,70 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models/schema"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("tg5uixgnz32nmqj")
+		if err != nil {
+			return err
+		}
+
+		// update
+		edit_collection := &schema.SchemaField{}
+		json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "sabinmy4",
+			"name": "collection",
+			"type": "relation",
+			"required": false,
+			"unique": false,
+			"options": {
+				"collectionId": "pejwlxcamufi2z9",
+				"cascadeDelete": false,
+				"minSelect": null,
+				"maxSelect": 1,
+				"displayFields": null
+			}
+		}`), edit_collection)
+		collection.Schema.AddField(edit_collection)
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("tg5uixgnz32nmqj")
+		if err != nil {
+			return err
+		}
+
+		// update
+		edit_collection := &schema.SchemaField{}
+		json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "sabinmy4",
+			"name": "collection",
+			"type": "relation",
+			"required": true,
+			"unique": false,
+			"options": {
+				"collectionId": "pejwlxcamufi2z9",
+				"cascadeDelete": false,
+				"minSelect": null,
+				"maxSelect": 1,
+				"displayFields": null
+			}
+		}`), edit_collection)
+		collection.Schema.AddField(edit_collection)
+
+		return dao.SaveCollection(collection)
+	})
+}
